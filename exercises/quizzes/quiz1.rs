08a028c93e4ff04f1a320b5de8bbb8fb6240fef7
zhuchentong <2037630@163.com>
2025-02-06 14:24:59 +0800

    update

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,12 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(count: i32) -> i32 { 
+    if count > 40 {
+        return 1 * count;
+    }
+    return 2 * count;
+ }
 
 fn main() {
     // You can optionally experiment here.
