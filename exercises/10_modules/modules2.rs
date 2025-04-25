ed017896508ed86a0881d236f0c9a7ac8d8467e2
zhuchentong <2037630@163.com>
2025-04-25 15:07:16 +0800

    update

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -6,12 +6,12 @@ mod delicious_snacks {
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
@@ -20,7 +20,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        delicious_snacks::fruits::PEAR as &str,
+        delicious_snacks::veggies::CUCUMBER as &str,
     );
 }
--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -8,12 +8,17 @@ use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
-    // let mut basket =
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket.
+    basket.insert(String::from("apple"), 4);
+    basket.insert(String::from("orange"), 6);
+    basket.insert(String::from("pear"), 1);
+    basket.insert(String::from("peach"), 5);
+    basket.insert(String::from("watermelon"), 2);
 
     basket
 }
